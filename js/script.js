ab7bd1785f2101305286dbfbf7fe0fbe864f2843
marcoche88 <marco.che@hotmail.it>
2021-07-05 15:53:17 +0200

    fixed script2

--- js/script.js
+++ js/script.js
@@ -5,18 +5,26 @@ per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
+var number = 0;
+var result;
 
 for (var i = 0; i < 100; i ++) {
     
-    var number = i + 1;
+    number = i + 1;
     
     if (number % 3 === 0 && number % 5 === 0) {
         console.log("FizzBuzz");
+        result += " FizzBuzz";
     } else if (number % 3 === 0) {
         console.log("Fizz");
+        result += " Fizz";
     } else if (number % 5 === 0) {
         console.log("Buzz");
+        result += " Buzz";
     } else {
         console.log(number);
+        result += " " + number.toString();
     }
 }
+
+document.write(result);
