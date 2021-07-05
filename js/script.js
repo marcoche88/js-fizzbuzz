442a8ba1c292f6c2f9eba4244bacb8918bcd837a
marcoche88 <marco.che@hotmail.it>
2021-07-05 15:17:00 +0200

    script

--- js/script.js
+++ js/script.js
@@ -4,3 +4,19 @@ ma per i multipli di 3 stampi “Fizz” al posto del numero e
 per i multipli di 5 stampi Buzz.
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
+
+
+for (var i = 0; i < 100; i ++) {
+    
+    var number = i + 1;
+    
+    if (number % 3 === 0 && number % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (number % 3 === 0) {
+        console.log("Fizz");
+    } else if (number % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(number);
+    }
+}
